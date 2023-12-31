da1b1f454fbcf369f1d6692f08aecd9c72fa67bf
ujaved <umarj.javed@gmail.com>
2023-12-30 20:24:02 -0400

    fix cli

--- cli.py
+++ cli.py
@@ -27,19 +27,19 @@ def get_qa_spec_prompt(user_spec: str, chat_session: CliSession, prompt_session:
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--model-id', type=str,
-                        required=True, choices=["gpt-4", "codellama_7b"])
+                        required=True, choices=["gpt-4", "codellama"])
     parser.add_argument('-v', '--validate', action='store_true')
     parser.add_argument('-t', '--terratest', action='store_true')
     args = parser.parse_args()
 
     download_terraform(os.getenv("TERRAFORM_VERSION"))
 
-    if args.model_id == "gpt-4":
-        chat_session = CliSession(id=0, terratest=args.terratest, validate_ctx=yaspin, generate_ctx=yaspin, chatbot=OpenAIChatbot(
-            model_id=os.getenv("OPENAI_MODEL_ID"), temperature=0, stream_handler_class=NoopStreamHandler))
-    elif args.model_id == "codellama_7b":
-        chat_session = CliSession(id=0, terratest=args.terratest, chatbot=LLamaChatbot(
-        ), validate_ctx=yaspin, generate_ctx=yaspin)
+    chatbot = OpenAIChatbot(model_id=os.getenv(
+        "OPENAI_MODEL_ID"), temperature=0, stream_handler_class=NoopStreamHandler)
+    if args.model_id == "codellama":
+        chatbot = LLamaChatbot()
+    chat_session = CliSession(id=0, terratest=args.terratest, validate_ctx=yaspin,
+                              generate_ctx=yaspin, chatbot=chatbot, static_validate=args.validate)
 
     prompt_session = PromptSession(lexer=PygmentsLexer(SqlLexer))
     user_spec = prompt_session.prompt(
@@ -53,8 +53,8 @@ def main():
                 prompt = prompt_session.prompt('> ')
             if args.terratest:
                 prompt += "\n Regenerate the terratest file."
-            chat_session.get_terraform_template(spec=prompt, validate=args.validate, **{
-                "text": "generating terraform template", "color": "green"})
+            chat_session.get_terraform_template(
+                spec=prompt, **{"text": "generating terraform template", "color": "green"})
             print(chat_session.terraform_template)
             prompt = prompt_session.prompt('> ')
         except KeyboardInterrupt:
